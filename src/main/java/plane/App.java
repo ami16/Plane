5bc0908350abd64b4498d8bc7c58eef4cdab98c2
bwstudio <bwstudio@ukr.net>
2016-09-08 18:57:56 +0300

    After f-cking Eve... No windows, just cmd

--- src/main/java/plane/App.java
+++ src/main/java/plane/App.java
@@ -1,10 +1,17 @@
 package plane;
 
+//import java.io.IOException;
+
 public class App {
 
    public App() {}
 
+//   public static void main(String[] args) throws IOException, InterruptedException {
    public static void main(String[] args) {
+      // windows only
+//      Process p = Runtime.getRuntime().exec("cmd c://java start cmd.exe");
+//      p.waitFor();
+
       Plane pl = new Plane() ;
       pl.fly();
    }
